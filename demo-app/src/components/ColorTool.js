c6a8284d5c14ba100ec5fcb65fa290d840b0dcfa
Eric Greene <eric@t4d.io>
2022-02-28 16:52:24 -0500

    add exercise 4

--- demo-app/src/components/ColorTool.js
+++ demo-app/src/components/ColorTool.js
@@ -1,14 +1,10 @@
 
 
-export const ColorTool = () => {
+export const ColorTool = (props) => {
 
-  const colors = [
-    { id: 1, name: 'red', hexcode: 'ff0000'},
-    { id: 2, name: 'green', hexcode: '00ff00'},
-    { id: 3, name: 'blue', hexcode: '0000ff'},
-  ];
 
-  const colorListItems = colors.map(color => <li key={color.id}>
+
+  const colorListItems = props.colors.map(color => <li key={color.id}>
     {color.name} {color.hexcode}
   </li>);
 
--- demo-app/src/index.js
+++ demo-app/src/index.js
@@ -3,9 +3,16 @@ import { render } from 'react-dom';
 import { ColorTool } from './components/ColorTool';
 import { CarTool } from './components/CarTool';
 
+const colorList = [
+  { id: 1, name: 'red', hexcode: 'ff0000'},
+  { id: 2, name: 'green', hexcode: '00ff00'},
+  { id: 3, name: 'blue', hexcode: '0000ff'},
+];
+
 render(
   <>
-    <ColorTool />
+    {/* React.createElement(ColorTool, { colors: colorList }); */}
+    <ColorTool colors={colorList} />
     <CarTool />
   </>,
   document.querySelector("#root"),
