f5719108194185a98b605b2777f350a6f20b607e
Eric Greene <eric@t4d.io>
2022-03-01 16:40:28 -0500

    add exercise 7

--- demo-app/src/components/ColorForm.js
+++ demo-app/src/components/ColorForm.js
@@ -2,67 +2,37 @@ import { useState } from 'react';
 
 export const ColorForm = (props) => {
 
-  // const formState = useState(
-  //   { name: '', hexcode: '' } // initial form state
-  // );
-
-  // // state data
-  // const colorForm = formState[0];
-
-  // // function to update the state data, and triggers the re-render
-  // const setColorForm = formState[1];
-
-  // array destructuring
   const [ colorForm, setColorForm ] = useState({ name: '', hexcode: ''});
 
-
-
-
-  // fn
-  // const change = (e) => {
-
-  //   // get the input element where the event fired
-  //   const inputElement = e.target; // target is the element where the change event fired
-
-  //   const valueTypedInByTheUser = inputElement.value;
-  //   const formControlName = inputElement.name;
-
-  //   // produced a new object reference and copied over the values from the
-  //   // original colorForm object
-  //   const newColorForm = {
-  //     name: colorForm.name,
-  //     hexcode: colorForm.hexcode,
-  //   };
-
-  //   newColorForm[formControlName] = valueTypedInByTheUser;
-
-  //   // update the state data, and triggers the re-render
-  //   setColorForm(newColorForm);
-
-  // };
-
-  const change = ({ target } /* object destructuring */) => {
+  const change = ({ target }) => {
     setColorForm({
-      ...colorForm, // object spread operator, copy all properties of the original object into the new one
-      // computed property
-      [target.name /* expression */]: target.type === "number"
+      ...colorForm,
+      [target.name]: target.type === "number"
         ? Number(target.value) : target.value,
     });
   };
 
-  console.log(colorForm);
+  const submitColor = () => {
+    props.onSubmitColor({ ...colorForm });
+
+    setColorForm({
+      name: '', hexcode: '',
+    });
+  }
 
   return (
     <form>
       <label>
         Name:
-        <input type="text" name="name" value={colorForm.name} onChange={change} />
+        <input type="text" name="name"
+          value={colorForm.name} onChange={change} />
       </label>
       <label>
         Hexcode:
-        <input type="text" name="hexcode" value={colorForm.hexcode} onChange={change} />
+        <input type="text" name="hexcode"
+          value={colorForm.hexcode} onChange={change} />
       </label>
-      <button type="button">Add Color</button>
+      <button type="button" onClick={submitColor}>Add Color</button>
     </form>
   )
 
