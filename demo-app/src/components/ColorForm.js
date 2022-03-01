30e4a071c95d9f718bbb8b5b6275be0fb3aa0a33
Eric Greene <eric@t4d.io>
2022-03-01 14:18:09 -0500

    add code for color form

--- demo-app/src/components/ColorForm.js
+++ demo-app/src/components/ColorForm.js
@@ -2,40 +2,52 @@ import { useState } from 'react';
 
 export const ColorForm = () => {
 
-  const formState = useState(
-    { name: '', hexcode: '' } // initial form state
-  );
+  // const formState = useState(
+  //   { name: '', hexcode: '' } // initial form state
+  // );
 
-  // state data
-  const colorForm = formState[0];
+  // // state data
+  // const colorForm = formState[0];
 
-  // function to update the state data, and triggers the re-render
-  const setColorForm = formState[1];
+  // // function to update the state data, and triggers the re-render
+  // const setColorForm = formState[1];
+
+  // array destructuring
+  const [ colorForm, setColorForm ] = useState({ name: '', hexcode: ''});
 
 
-  
+
 
   // fn
-  const change = (e) => {
+  // const change = (e) => {
 
-    // get the input element where the event fired
-    const inputElement = e.target; // target is the element where the change event fired
+  //   // get the input element where the event fired
+  //   const inputElement = e.target; // target is the element where the change event fired
 
-    const valueTypedInByTheUser = inputElement.value;
-    const formControlName = inputElement.name;
+  //   const valueTypedInByTheUser = inputElement.value;
+  //   const formControlName = inputElement.name;
 
-    // produced a new object reference and copied over the values from the
-    // original colorForm object
-    const newColorForm = {
-      name: colorForm.name,
-      hexcode: colorForm.hexcode,
-    };
+  //   // produced a new object reference and copied over the values from the
+  //   // original colorForm object
+  //   const newColorForm = {
+  //     name: colorForm.name,
+  //     hexcode: colorForm.hexcode,
+  //   };
 
-    newColorForm[formControlName] = valueTypedInByTheUser;
+  //   newColorForm[formControlName] = valueTypedInByTheUser;
 
-    // update the state data, and triggers the re-render
-    setColorForm(newColorForm);
+  //   // update the state data, and triggers the re-render
+  //   setColorForm(newColorForm);
 
+  // };
+
+  const change = ({ target } /* object destructuring */) => {
+    setColorForm({
+      ...colorForm, // object spread operator, copy all properties of the original object into the new one
+      // computed property
+      [target.name /* expression */]: target.type === "number"
+        ? Number(target.value) : target.value,
+    });
   };
 
   console.log(colorForm);
